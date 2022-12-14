8b428c676cde78ddbebfe88e06cf113ad89a6eae
Lisa Copeland <lisa.copeland@betterworks.com>
2022-12-14 13:56:50 -0500

    updated style

--- vuejs-primevue/src/views/FindMentor.vue
+++ vuejs-primevue/src/views/FindMentor.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="find">
-    <div op80 text-lg fw300 m1 w100>
+  <div class="wrapper">
+    <div text-lg fw300>
       <h1>Find a Betterworks Mentor</h1>
     </div>
-    <div op30 text-lg fw300 m1>
+    <div text-lg fw300>
       <span class="p-float-label">
         <InputText id="search" type="text" />
-        <label for="search">Search</label>
       </span>
       <Button label="Search" />
     </div>
@@ -15,10 +14,10 @@
 
 <style>
 @media (min-width: 1024px) {
-  .find {
-    min-height: 100vh;
+  .wrapper {
     display: flex;
-    align-items: center;
+    place-items: flex-start;
+    flex-wrap: wrap;
   }
 }
 </style>
